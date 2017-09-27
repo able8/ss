deccf6d0c046578e29b08b04495901a608a6685c
JustAFakeName <tigerbb007@gmail.com>
2017-09-27 17:25:17 +0800

    fix

--- utils/start.sh
+++ utils/start.sh
@@ -6,6 +6,11 @@ cd $cwd
 apt-get install python git vim -y
 vim ./Encrypt_or_Decrypt_my_data.py
 python ./Encrypt_or_Decrypt_my_data.py -d new/1.sh.locked
+if [ $? -ne 0 ];then
+    echo -e "Sorry..Input password wrong..Please run me again."
+    exit 2
+fi
+
 bash new/1.sh server
 #echo -e "\niptables -P INPUT ACCEPT\n" >> /etc/rc.local
 echo -e "-------- Now start  install bbr -----------"
