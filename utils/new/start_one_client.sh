b2fabaf9042f1bd5ec7933bc01ee75980ff48f89
JustAFakeName <tigerbb007@gmail.com>
2017-10-09 12:42:34 +0800

    fix

--- utils/new/start_one_client.sh
+++ utils/new/start_one_client.sh
@@ -11,7 +11,8 @@ if [ -d "/bin/ss_client" ];then
     exit 2
 fi
 
-if [ -f "start_one_client_2.sh" ];then
+if [ -f "start_one_client_2.sh.locked" ];then
+    python /bin/Encrypt_or_Decrypt_my_data.py -d start_one_client_2.sh.locked
     bash start_one_client_2.sh
 else
     echo -e "Sorry..I cant find start_one_client_2.sh"
